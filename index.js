41e4d5ac63dddd39c2deeaa37176b887e739ee21
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-26 20:40:57 -0700

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -8,29 +8,31 @@ var inherits = require('util').inherits
 var retry = require('retry')
 var url = require('url')
 
-function HTTPCallback(options) {
-  options = ( options || {} )
-  // Private object containing options passed to `retry.optionation()` for each
-  // attempt to call back to registered HTTP endpoint. Endpoints that fail
-  // after retries are deregistered.
-  this._retryOptions =  ( 'retry' in options ? options.retry : { } )
+function HTTPCallback (options) {
+  options = options || {}
+  // Private object containing options passed to `retry.optionation()`
+  // for each attempt to call back to registered HTTP endpoint.
+  // Endpoints that fail after retries are deregistered.
+  this._retryOptions = 'retry' in options ? options.retry : {}
 
-  // Private object map from string from `url.parse(value).href` to object from
-  // `url.parse(value)` describing HTTP endpoints registered for callbacks.
-  this._listeners = { }
+  // Private object map from string from `url.parse(value).href` to
+  // object from `url.parse(value)` describing HTTP endpoints registered
+  // for callbacks.
+  this._listeners = {}
 
   // HTTPCallbacks are EventEmitters.
-  EventEmitter.call(this) }
+  EventEmitter.call(this)
+}
 
 inherits(HTTPCallback, EventEmitter)
 
 var prototype = HTTPCallback.prototype
 
-// An HTTP request handler that registers HTTP callback endpoints. Suitable for
-// use with the core Node.js HTTP module.
-prototype.handler = function(request, response) {
+// An HTTP request handler that registers HTTP callback endpoints.
+// Suitable for use with the core Node.js HTTP module.
+prototype.handler = function (request, response) {
   var self = this
-  request.pipe(concat(function(body) {
+  request.pipe(concat(function (body) {
     var parsedURL = parseBody(body.toString())
     var href = parsedURL.href
     if (validBody(parsedURL)) {
@@ -40,92 +42,118 @@ prototype.handler = function(request, response) {
       response.statusCode = 202
       response.end()
       // Emit an event.
-      self.emit('registration', parsedURL) }
-    else {
+      self.emit('registration', parsedURL)
+    } else {
       // Respond 400.
       response.statusCode = 400
       response.end('Invalid URL')
       // Emit an event.
-      self.emit('badrequest', parsedURL) } })) }
+      self.emit('badrequest', parsedURL)
+    }
+  }))
+}
 
-// Helper function to parse HTTP request bodies sent to endpoints to register
-// callback URLS.
-function parseBody(string) {
+// Helper function to parse HTTP request bodies sent to endpoints to
+// register callback URLS.
+function parseBody (string) {
   // Freeze the object, to prevent event listeners from mutating it.
-  return Object.freeze(url.parse(string)) }
+  return Object.freeze(url.parse(string))
+}
 
-// Helper function to check whether an HTTP request body with a callback URL
-// includes all the URL components needed to make callback HTTP requests.
-function validBody(parsedURL) {
+// Helper function to check whether an HTTP request body with a callback
+// URL includes all the URL components needed to make callback HTTP
+// requests.
+function validBody (parsedURL) {
   var protocol = parsedURL.protocol
   return (
-    ( protocol === 'https:' || protocol === 'http:' ) &&
-    parsedURL.hostname ) }
+    (protocol === 'https:' || protocol === 'http:') &&
+    parsedURL.hostname
+  )
+}
 
-// Send data to all registered HTTP callback endpoints, passing a function that
-// will write data to the bodies of requests sent.
-prototype.send = function(dataCallback) {
+// Send data to all registered HTTP callback endpoints, passing a
+// function that will write data to the bodies of requests sent.
+prototype.send = function (dataCallback) {
   var self = this
-  self._forEachListener(function(listener) {
-    self._sendDataToListener(dataCallback, listener, function(error) {
+  self._forEachListener(function (listener) {
+    self._sendDataToListener(dataCallback, listener, function (error) {
       if (error) {
         self.emit('failure', error)
-        self._deregister(listener.href) } }) }) }
+        self._deregister(listener.href)
+      }
+    })
+  })
+}
 
-// Helper function to call back to a specific HTTP endpoint, retrying according
-// to configuration and emitting events.
-prototype._sendDataToListener = function(dataCallback, listener, errback) {
+// Helper function to call back to a specific HTTP endpoint, retrying
+// according to configuration and emitting events.
+prototype._sendDataToListener = function (dataCallback, listener, errback) {
   var self = this
-  var useHTTPS = ( listener.protocol === 'https:' )
-  var protocol = ( useHTTPS ? https : http )
+  var useHTTPS = listener.protocol === 'https:'
+  var protocol = useHTTPS ? https : http
   var operation = retry.operation(self._retryOptions)
-  operation.attempt(function(count) {
+  operation.attempt(function (count) {
     self.emit('attempt', listener.href, count)
     var options = parsedURLToRequestOptions(listener, useHTTPS)
     var request = protocol.request(options)
-      .once('response', function(response) {
-        if (successfulCallbackResponse(response)) {
-          self.emit('success', listener.href)
-          errback() }
-        else {
-          var message = (
-            'Unsuccessful request ' +
-            '(status code ' + response.statusCode + ')' )
-          var error = new Error(message)
-          if (!operation.retry(error)) {
-            errback(operation.mainError()) } } })
-      .once('error', function(error) {
+    .once('response', function (response) {
+      if (successfulCallbackResponse(response)) {
+        self.emit('success', listener.href)
+        errback()
+      } else {
+        var message = (
+          'Unsuccessful request ' +
+          '(status code ' + response.statusCode + ')'
+        )
+        var error = new Error(message)
         if (!operation.retry(error)) {
-          errback(operation.mainError()) } })
-    dataCallback(request) }) }
+          errback(operation.mainError())
+        }
+      }
+    })
+    .once('error', function (error) {
+      if (!operation.retry(error)) {
+        errback(operation.mainError())
+      }
+    })
+    dataCallback(request)
+  })
+}
 
 // Helper function to check whether a callback request was successful.
-function successfulCallbackResponse(response) {
+function successfulCallbackResponse (response) {
   var statusCode = response.statusCode
-  return ( statusCode >= 200 && statusCode < 300) }
+  return statusCode >= 200 && statusCode < 300
+}
 
 // Helper functionto iterate registered HTTP callback endpoints.
-prototype._forEachListener = function(callback) {
+prototype._forEachListener = function (callback) {
   var listeners = this._listeners
-  Object.keys(listeners).forEach(function(href) {
-    callback(listeners[href]) }) }
+  Object.keys(listeners).forEach(function (href) {
+    callback(listeners[href])
+  })
+}
 
-// Helper function to convert objects created with url.parse into options
-// arguments for http.request when sending callback requests.
-function parsedURLToRequestOptions(parsedURL, useHTTPS) {
+// Helper function to convert objects created with url.parse into
+// options arguments for http.request when sending callback requests.
+function parsedURLToRequestOptions (parsedURL, useHTTPS) {
   return {
     auth: parsedURL.auth,
     host: parsedURL.hostname,
     method: 'POST',
-    path: ( parsedURL.pathname || ( useHTTPS ? 443 : 80 ) ),
-    port: ( parsedURL.port || 80 ),
-    query: parsedURL.query } }
+    path: parsedURL.pathname || (useHTTPS ? 443 : 80),
+    port: parsedURL.port || 80,
+    query: parsedURL.query
+  }
+}
 
 // Helper function to deregister an HTTP callback endpoint.
-prototype._deregister = function(href) {
+prototype._deregister = function (href) {
   delete this._listeners[href]
-  this.emit('deregistration', href) }
+  this.emit('deregistration', href)
+}
 
 // Returns an array of all HREFs of registered callback endpoints.
-prototype.callbackListeners = function() {
-  return Object.keys(this._listeners) }
+prototype.callbackListeners = function () {
+  return Object.keys(this._listeners)
+}
--- test.js
+++ test.js
@@ -5,128 +5,170 @@ var series = require('async-series')
 var tape = require('tape')
 var url = require('url')
 
-tape(function(test) {
+tape(function (test) {
   test.plan(9)
-  // The data to send from an event source server to an event listener server.
+  // The data to send from an event source server to an event listener
+  // server.
   var CALLBACK_DATA = 'callback body'
   var BAD_CALLBACK_PORT = 1
-  var BAD_CALLBACK = ( 'http://localhost:' + BAD_CALLBACK_PORT + '/x' )
+  var BAD_CALLBACK = 'http://localhost:' + BAD_CALLBACK_PORT + '/x'
   var RETRY_INTERVAL = 500
   var listenerURL
   // Create an event source server with an example HTTPCallback.
   var example = new HTTPCallback(
-    // Configure automatic retry so the event source server will retry and
-    // deregister bad callbacks quickly.
-    { retry: {
+    // Configure automatic retry so the event source server will retry
+    // and deregister bad callbacks quickly.
+    {
+      retry: {
         maxTimeout: RETRY_INTERVAL,
         minTimeout: RETRY_INTERVAL,
         random: false,
-        retries: 0 } })
+        retries: 0
+      }
+    }
+  )
   example
-    .once('registration', function(parsedURL) {
+  .once('registration', function (parsedURL) {
+    test.equal(
+      parsedURL.pathname, '/receive',
+      'emits registration event for good callback'
+    )
+    this
+    .once('registration', function (parsedURL) {
       test.equal(
-        parsedURL.pathname, '/receive',
-        'emits registration event for good callback')
-      this
-        .once('registration', function(parsedURL) {
-          test.equal(
-            parsedURL.pathname, '/x',
-            'emits registration event for bad callback') }) })
-    .on('failure', function(error) {
-      test.equal(
-        error.errno, 'ECONNREFUSED',
-        'emits failure event for bad callback')
-      test.equal(
-        error.port, BAD_CALLBACK_PORT,
-        'failure event is for the bad port') })
-    .once('deregistration', function(href) {
-      test.equal(
-        href, BAD_CALLBACK,
-        'deregistration event for bad callback') })
+        parsedURL.pathname, '/x',
+        'emits registration event for bad callback')
+    })
+  })
+  .on('failure', function (error) {
+    test.equal(
+      error.errno, 'ECONNREFUSED',
+      'emits failure event for bad callback'
+    )
+    test.equal(
+      error.port, BAD_CALLBACK_PORT,
+      'failure event is for the bad port'
+    )
+  })
+  .once('deregistration', function (href) {
+    test.equal(
+      href, BAD_CALLBACK,
+      'deregistration event for bad callback'
+    )
+  })
   var source = http.createServer(
-    function(request, response) {
+    function (request, response) {
       // The event source server proxies POST /register to the
       // HTTPCallback request handler.
       var callbackRequest = (
         request.method === 'POST' &&
-        url.parse(request.url).pathname === '/register' )
+        url.parse(request.url).pathname === '/register'
+      )
       if (callbackRequest) {
-        example.handler(request, response) }
+        example.handler(request, response)
       // Otherwise it fails with an error.
-      else {
-         throw new Error() } })
-    // Start the event source server on a random high port.
-    .listen(0, function() {
-      // Record the random port for future reference.
-      var sourcePort = this.address().port
-      // Create an event listener server.
-      var listener = http.createServer(
-        // The event listener server responds to POST /receive.
-        function(request, response) {
-          var callbackRequest = (
-            request.method === 'POST' &&
-            url.parse(request.url).pathname === '/receive' )
-          if (callbackRequest) {
-            // Read the the body of the POST request.
-            request.pipe(concat(function(buffer) {
-              var asString = buffer.toString()
-              // Should equal the value sent by the even source server.
-              test.equal(
-                asString, CALLBACK_DATA,
-                'listener receives data via POST /receive')
-              response.end()
-              setTimeout(
-                function() {
-                  test.deepEqual(
+      } else {
+        throw new Error()
+      }
+    }
+  )
+  // Start the event source server on a random high port.
+  .listen(0, function () {
+    // Record the random port for future reference.
+    var sourcePort = this.address().port
+    // Create an event listener server.
+    var listener = http.createServer(
+      // The event listener server responds to POST /receive.
+      function (request, response) {
+        var callbackRequest = (
+          request.method === 'POST' &&
+          url.parse(request.url).pathname === '/receive'
+        )
+        if (callbackRequest) {
+          // Read the the body of the POST request.
+          request.pipe(concat(function (buffer) {
+            var asString = buffer.toString()
+            // Should equal the value sent by the even source server.
+            test.equal(
+              asString, CALLBACK_DATA,
+              'listener receives data via POST /receive'
+            )
+            response.end()
+            setTimeout(
+              function () {
+                test.deepEqual(
                   example.callbackListeners(),
-                  [ listenerURL ],
-                  'only the listener remains listening')
-                  // Close our test servers.
-                  source.close()
-                  listener.close() },
-                RETRY_INTERVAL ) })) }
-          // Otherwise it fails with an error.
-          else {
-             throw new Error() } })
-        // Also start the event listener server on a random high port.
-        .listen(0, function() {
-          // Record that random port, so we can tell the event source server
-          // what port to call back on.
-          var listenerPort = this.address().port
-          listenerURL =  ( 'http://localhost:' + listenerPort + '/receive')
-          var post = {
-            port: sourcePort,
-            path: '/register',
-            method: 'POST' }
-          series(
-            [ function(done) {
-                http
-                  .request(post)
-                  .once('response', function(response) {
-                    test.equal(
-                      response.statusCode, 202,
-                      'POST /register to source responds 202 for good callback')
-                    done() })
-                  .once('error', done)
-                  // The body of the callback registration request to the event
-                  // source server is the plain-text URL of the source listener
-                  // server where the event source server should POST data.
-                  .end(listenerURL) },
-              function(done) {
-                http
-                  .request(post)
-                  .once('response', function(response) {
-                    test.equal(
-                      response.statusCode, 202,
-                      'POST /register to source responds 202 for bad callback')
-                    done() })
-                  .once('error', done)
-                  // Register a bogus callback as well, to test error and
-                  // deregistration events.
-                  .end(BAD_CALLBACK) },
-              function() {
-                // Dispatch the callback data to all listeners registered with
-                // the event source server.
-                example.send(function(stream) {
-                  stream.end(CALLBACK_DATA) }) } ],
-            test.ifError.bind(test)) }) }) })
+                  [listenerURL],
+                  'only the listener remains listening'
+                )
+                // Close our test servers.
+                source.close()
+                listener.close()
+              },
+              RETRY_INTERVAL
+            )
+          }))
+        // Otherwise it fails with an error.
+        } else {
+          throw new Error()
+        }
+      }
+    )
+    // Also start the event listener server on a random high port.
+    .listen(0, function () {
+      // Record that random port, so we can tell the event source
+      // server what port to call back on.
+      var listenerPort = this.address().port
+      listenerURL = 'http://localhost:' + listenerPort + '/receive'
+      var post = {
+        port: sourcePort,
+        path: '/register',
+        method: 'POST'
+      }
+      series(
+        [
+          function (done) {
+            http.request(post)
+            .once('response', function (response) {
+              test.equal(
+                response.statusCode, 202,
+                'POST /register to source' +
+                ' responds 202 for good callback'
+              )
+              done()
+            })
+            .once('error', done)
+            // The body of the callback registration request to the
+            // event source server is the plain-text URL of the
+            // source listener server where the event source server
+            // should POST data.
+            .end(listenerURL)
+          },
+          function (done) {
+            http.request(post)
+            .once('response', function (response) {
+              test.equal(
+                response.statusCode, 202,
+                'POST /register to source' +
+                ' responds 202 for bad callback'
+              )
+              done()
+            })
+            .once('error', done)
+            // Register a bogus callback as well, to test error and
+            // deregistration events.
+            .end(BAD_CALLBACK)
+          },
+          function () {
+            // Dispatch the callback data to all listeners
+            // registered with the event source server.
+            example.send(function (stream) {
+              stream.end(CALLBACK_DATA)
+            })
+          }
+        ],
+        test.ifError.bind(test)
+      )
+    })
+  })
+})
